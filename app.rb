01a9de3e9a658f7af7d8e59a990052f2c551c3b0
Michael Chen <michaelchengaming@gmail.com>
2019-07-22 23:28:12 -0700

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
+	end
+
+	get '/info' do
+		erb :info
+		# "Testing the info page"
+	end
+
+	get '/boom' do
+		erb :boom
 	end
 end
